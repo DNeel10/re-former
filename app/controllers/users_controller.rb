466dc3635031dbe3e09a657713f214e4a9f40ab5
Daniel Neel <dneel10@gmail.com>
2023-05-22 23:53:47 -0400

    update new and edit views to play with error display. update user controller to play with routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user)
     else
       render :new, status: :unprocessable_entity
     end
